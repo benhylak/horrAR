1829d921c90b13f3cf183f313f7901e5824c2f67
Benjamin Hylak <bhylak@gmail.com>
2018-11-25 13:28:59 -0500

    Finished initial chucky prototype. Capable of shooting/killing chucky

---
+++ Assets/_game/Scripts/ChuckyBehavior.cs
@@ -0,0 +1,142 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.MagicLeap;
+using System.Threading;
+using System.Threading.Tasks;
+
+public class ChuckyBehavior : MonoBehaviour {
+
+	public GameObject head;
+	public Camera camera;
+	
+	public bool LookAt = false;
+	public bool outOfRange = false;
+
+	public Quaternion lastRotation = Quaternion.identity;
+
+	private ChuckyBehaviorState currentState = ChuckyBehaviorState.IDLE;
+	private Animator _anim;
+
+	private float timeOfLastTrigger = 0f;
+
+	private bool justEntered = true;
+
+	public enum ChuckyBehaviorState
+	{
+		IDLE = 0,
+		IDLE_LOOKING = 1,
+		IDLE_DID_WAVE = 2,
+		RUNNING = 3,
+		FIGHTING = 4,
+		DEAD = 5
+	}
+
+	// Use this for initialization
+	void Start () {
+		_anim = GetComponent<Animator>();
+
+		MLInput.OnControllerButtonDown += (byteVal, button) =>
+		{
+			if(button == MLInputControllerButton.Bumper)
+			{
+				if(currentState == ChuckyBehaviorState.IDLE_DID_WAVE)
+				{
+					currentState = ChuckyBehaviorState.RUNNING;
+					_anim.SetBool("running", true);
+				}
+				else
+				{
+					currentState = ChuckyBehaviorState.IDLE;
+					LookAt = false;
+					_anim.SetBool("running", false);
+					_anim.SetTrigger("default");
+				}
+			}
+		};
+	}
+
+	private void TryLookAtCamera()
+	{
+		var dir = camera.transform.position - head.transform.position;
+		var targetRot = Quaternion.LookRotation(dir) * Quaternion.Euler(15f, 0, 0);
+		//targetRot *= Quaternion.Euler(0f, -90f, -90f); //flip for orientation
+
+		//constrain
+		var identity = (head.transform.parent != null) ? head.transform.parent.rotation : Quaternion.identity;
+		var a = Quaternion.Angle(identity, targetRot);
+
+		//deadband of 60<->90, prevents flickering
+		if(a > 90f) outOfRange = true;
+		else if(a < 60f) outOfRange = false;
+		
+		outOfRange = false;
+
+		if (!outOfRange)
+		{
+			if(lastRotation == Quaternion.identity) //initialize lastRotation if a perfect identity quaternion
+			{
+				lastRotation = head.transform.rotation;
+			}
+
+			lastRotation = Quaternion.Slerp(lastRotation, targetRot, 0.3f);
+			head.transform.rotation = lastRotation;
+		}
+	}
+
+	public void Hit()
+	{
+		if(currentState == ChuckyBehaviorState.RUNNING)
+		{
+			currentState = ChuckyBehaviorState.FIGHTING;
+			_anim.SetBool("running", false);
+			_anim.SetTrigger("agony");
+			LookAt = false;
+		}
+	}
+
+	void LateUpdate()
+	{
+		if(LookAt) TryLookAtCamera();
+	
+		if(!LookAt || outOfRange)
+		{
+			lastRotation = Quaternion.Slerp(lastRotation, head.transform.rotation, 0.2f);
+		}
+
+		head.transform.rotation = lastRotation;
+	}
+	
+	// Update is called once per frame
+	async void Update () {
+		
+		var directLookVec = head.transform.position - camera.transform.position;
+
+		if(currentState == ChuckyBehaviorState.RUNNING)
+		{
+			this.transform.position += this.transform.forward * 0.4f *  Time.deltaTime;
+		}
+		else if(Vector3.Dot(directLookVec, camera.transform.forward) > 0.8f && justEntered && Time.time - timeOfLastTrigger > 2.5f)
+		{
+			switch(currentState)
+			{
+				case ChuckyBehaviorState.IDLE:
+					await Task.Delay(1500);
+					LookAt = true;
+					currentState = ChuckyBehaviorState.IDLE_LOOKING;
+					break;
+
+				case ChuckyBehaviorState.IDLE_LOOKING:
+					await Task.Delay(750);
+
+					_anim.SetTrigger("wave");
+					currentState = ChuckyBehaviorState.IDLE_DID_WAVE;
+					break;
+			}
+
+			timeOfLastTrigger = Time.time;
+			justEntered = false;
+		}
+		else if(Vector3.Dot(directLookVec, camera.transform.forward) < 0.73f) justEntered = true;
+	}
+}
---
+++ Assets/_game/Scripts/ExperienceManager.cs
@@ -0,0 +1,113 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.MagicLeap;
+
+public class ExperienceManager : MonoBehaviour {
+	GameObject _objectBeingPlaced = null;
+	public Transform _controller;
+
+	LineRenderer _lineRenderer; 
+
+	public GameObject controllerDisplay;
+
+	bool canMoveItems = true;
+
+	public Camera _camera;
+
+	float _tractorBeamDist = 0f;
+
+	[Tooltip("Put Chucky and TV in here")]
+	public LayerMask moveLayer;
+
+	// Use this for initialization
+	void Start () {
+		_lineRenderer = GetComponent<LineRenderer>();
+
+		MLInput.OnControllerButtonDown += (byteData, button) =>
+		{
+			if(button == MLInputControllerButton.HomeTap)
+			{
+				canMoveItems = !canMoveItems;
+				controllerDisplay.SetActive(canMoveItems);
+			}
+		};
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		bool triggerDown = MLInput.GetController(0).TriggerValue > MLInput.TriggerDownThreshold;
+		
+		if(canMoveItems && triggerDown && _objectBeingPlaced == null)
+		{
+			// Virtual Raycast
+			Ray ray = new Ray(_controller.position, _controller.forward);
+			RaycastHit result;
+
+			if (Physics.Raycast(ray, out result, 100, moveLayer))
+			{
+				_objectBeingPlaced = result.collider.gameObject;
+				_tractorBeamDist = Vector3.Distance(_controller.position, _objectBeingPlaced.transform.position);
+				_lineRenderer.positionCount = 2;
+			}
+			//try raycast
+			//if you hit an item
+		}
+		else if(canMoveItems && triggerDown)
+		{
+			Vector3 tractorBeamEndPos = _controller.position + _controller.forward * _tractorBeamDist;
+			
+			_objectBeingPlaced.transform.position = tractorBeamEndPos;
+			_objectBeingPlaced.transform.forward = -_controller.forward;
+
+			_lineRenderer.SetPosition(0, _objectBeingPlaced.transform.position);
+			_lineRenderer.SetPosition(1, _controller.transform.position);
+		}
+		else if(_objectBeingPlaced != null) 
+		{
+			_lineRenderer.positionCount = 0;
+			_objectBeingPlaced = null;
+		}
+	}
+
+	public void OnRaycastHit(MLWorldRays.MLWorldRaycastResultState state, RaycastHit result, float confidence)
+	{
+		// if(!_frozen)
+		// {
+		// 	if (state != MLWorldRays.MLWorldRaycastResultState.RequestFailed && state != MLWorldRays.MLWorldRaycastResultState.NoCollision)
+		// 	{
+		// 		// Update the cursor position and normal.
+		// 		transform.position = result.point;
+		// 		transform.LookAt(result.normal + result.point);
+		// 		transform.localScale = Vector3.one;
+
+		// 		// Set the color to yellow if the hit is unobserved.
+		// 		_render.material.color = (state == MLWorldRays.MLWorldRaycastResultState.HitObserved)? _color : Color.yellow;
+
+		// 		if (_scaleWhenClose)
+		// 		{
+		// 			// Check the hit distance.
+		// 			if (result.distance < 1.0f)
+		// 			{
+		// 				// Apply a downward scale to the cursor.
+		// 				transform.localScale = new Vector3(result.distance, result.distance, result.distance);
+		// 			}
+		// 		}
+
+		// 		_hit = true;
+		// 	}
+		// 	else
+		// 	{
+		// 		// Update the cursor position and normal.
+		// 		transform.position = (_raycast.RayOrigin + (_raycast.RayDirection * _defaultDistance));
+		// 		transform.LookAt(_raycast.RayOrigin);
+		// 		transform.localScale = Vector3.one;
+
+		// 		_render.material.color = Color.red;
+
+		// 		_hit = false;
+		// 	}
+		// }
+	}
+}
